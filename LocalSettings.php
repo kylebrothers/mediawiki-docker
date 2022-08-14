404f6fd76e87bac63d6f49a71f2a7b9dbe35fe60
kylebrothers <43184070+kylebrothers@users.noreply.github.com>
2022-08-14 13:54:05 -0400

    FixedHead file location

--- LocalSettings.php
+++ LocalSettings.php
@@ -132,7 +132,7 @@ $wgDiff3 = "/usr/bin/diff3";
 #$wgDefaultSkin = "vector";
 wfLoadSkin( 'chameleon' );
 $wgDefaultSkin= 'chameleon';
-$egChameleonLayoutFile=__DIR__ . '/var/www/html/skins/chameleon/layouts/fixedhead.xml';
+$egChameleonLayoutFile=__DIR__ . '/skins/chameleon/layouts/fixedhead.xml';
 
 
 # Enabled skins.
