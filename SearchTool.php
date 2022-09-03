634e48a58c2fb97b50b9ed23efa471a8abc52968
kylebrothers <43184070+kylebrothers@users.noreply.github.com>
2022-09-03 17:51:26 -0400

    Add my custom search tool extensions

---
+++ SearchTool.php
@@ -0,0 +1,36 @@
+<?php
+ 
+$wgExtensionFunctions[] = "wfSearchTool";
+ 
+$wgExtensionCredits['parserhook'][] =
+  array(
+        'name'        => 'SearchTool',
+        'author'      => array( 'The MediaWiki Community', 'Dan Bolser' ),
+        'url'         => 'http://www.mediawiki.org/wiki/Extension:Google',
+        'description' => 'Allow inclusion of a multiple searches.',
+        );
+ 
+function wfSearchTool() {
+  global $wgParser;
+ 
+  $wgParser->setHook( "SearchTool", "wfRenderGoogleSearch" );
+}
+ 
+# The callback function for converting the input text to HTML output                                                                    
+function wfRenderGoogleSearch( $input, $args, $parser ) {
+  $site = $args['site'] ;
+  $submit = $args['submit'] ;
+
+  $output  = '<!-- Search the indicated site -->';
+  $output .= '<form method="GET" action="'. $site. '">';
+  $output .= '<div class="col-sm-8">';
+  $output .= '        <input type="text" class="form-control" name="q" value="'. $input. '"/>';
+  $output .= '</div>';
+  $output .= '<div class="col-sm-4">';
+  $output .= '        <input class="btn btn-default btn-block"  type="submit" value="'. $submit. '"/>';
+  $output .= '</div>';
+  $output .= '</form>';
+ # $output .= '</center>';
+  $output .= '<!-- Search Google -->';
+  return $output;
+}
---
+++ SearchWikipedia.php
@@ -0,0 +1,32 @@
+<?php
+ 
+$wgExtensionFunctions[] = "wfSearchWikipedia";
+ 
+$wgExtensionCredits['parserhook'][] =
+  array(
+        'name'        => 'SearchWikipedia',
+        'author'      => array( 'The MediaWiki Community', 'Dan Bolser' ),
+        'url'         => 'http://www.mediawiki.org/wiki/Extension:Google',
+        'description' => 'Allow inclusion of a wikipedia searches.',
+        );
+ 
+function wfSearchWikipedia() {
+  global $wgParser;
+ 
+  $wgParser->setHook( "SearchWikipedia", "wfRenderWikipediaSearch" );
+}
+ 
+# The callback function for converting the input text to HTML output                                                                    
+function wfRenderWikipediaSearch( $input, $args, $parser ) {
+  $output  = '<!-- Search the indicated site -->';
+  $output .= '<form action="http://en.wikipedia.org/wiki/Special:Search" id="searchform">';
+  $output .= '<div class="col-sm-8">';
+  $output .= '        <input title="Search Wikipedia [f]" accesskey="f" value="" type="text" class="form-control" name="search"/>';
+  $output .= '</div>';
+  $output .= '<div class="col-sm-4">';
+  $output .= '            <input class="btn btn-default btn-block"  name="go" type="submit" value="Wikipedia"/>';
+  $output .= '</div>';
+  $output .= '</form>';
+  $output .= '<!-- Search Google -->';
+  return $output;
+}
